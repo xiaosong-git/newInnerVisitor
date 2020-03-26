d19a512855d9c4d6e8eb88f1ee288eeb2cd98030
jifei <jifeiyu338@163.com>
2020-03-26 15:43:52 +0800

    企业版与上位机交互

--- src/main/java/com/xiaosong/validate/foreign/ForeginValidator.java
+++ src/main/java/com/xiaosong/validate/foreign/ForeginValidator.java
@@ -4,6 +4,7 @@ import com.jfinal.core.Controller;
 import com.jfinal.kit.Ret;
 import com.jfinal.plugin.activerecord.Db;
 import com.jfinal.plugin.activerecord.Record;
+import com.jfinal.plugin.ehcache.CacheKit;
 import com.jfinal.validate.Validator;
 import com.xiaosong.RSA.RSAEncrypt;
 import com.xiaosong.compose.Result;
@@ -22,18 +23,22 @@ public class ForeginValidator extends Validator {
         setRet(Ret.fail("sign","fail"));
         validateRequiredString("pospCode", "desc", "请输入上位机码");
         validateRequiredString("orgCode", "desc", "请输入大楼编码");
-        validateRequiredString("pageNum", "desc", "请输入页数");
-        validateRequiredString("pageSize", "desc", "请输入每页范围显示条数");
         validateRequiredString("mac", "desc", "请输入校验码");
-        VKey key = VKey.dao.findFirst(Db.getSql("foreign.findOrgCode"), c.get("pospCode"), c.get("orgCode"));
-        if (key==null) {
-            addError("desc", "未通过pospCode与orgCode找到上位机！");
-        }
-        try {
-            byte[] macs = RSAEncrypt.decrypt(RSAEncrypt.loadPublicKeyByStr(key.getPublicKey()), Base64.decodeBase64(c.get("mac")));
-            String str = new String(macs);
-        } catch (Exception e) {
-            e.printStackTrace();
+        String mac = CacheKit.get("KEY", c.get("orgCode") + c.get("pospCode"));
+        if (mac==null){
+            VKey key = VKey.dao.findFirst(Db.getSql("foreign.findOrgCode"), c.get("pospCode"), c.get("orgCode"));
+            if (key==null) {
+                addError("desc", "未通过pospCode与orgCode找到上位机！");
+            }
+            try {
+                byte[] macs = RSAEncrypt.decrypt(RSAEncrypt.loadPublicKeyByStr(key.getPublicKey()), Base64.decodeBase64(c.get("mac")));
+                String str = new String(macs);
+                CacheKit.put("KEY",c.get("orgCode") + c.get("pospCode"),c.get("mac"));
+            } catch (Exception e) {
+                e.printStackTrace();
+                addError("desc", "校验错误！"+e);
+            }
+        }else if (!mac.equals(c.get("mac")) ){
             addError("desc", "校验错误！");
         }
 
