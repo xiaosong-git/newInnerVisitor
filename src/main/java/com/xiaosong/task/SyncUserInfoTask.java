56f2b3776add532429a932b05b017ae2ba9de522
chennl <373117208@qq.com>
2020-09-22 09:57:24 +0800

    单点登录

---
+++ src/main/java/com/xiaosong/task/SyncUserInfoTask.java
@@ -0,0 +1,40 @@
+package com.xiaosong.task;
+
+import com.jfinal.plugin.activerecord.Db;
+import com.jfinal.plugin.activerecord.Record;
+import com.xiaosong.common.api.websocket.WebSocketSyncData;
+import com.xiaosong.common.web.sso.SSOService;
+import com.xiaosong.constant.TableList;
+import com.xiaosong.model.VSysUser;
+
+import java.util.List;
+
+/**
+ * Created by CNL on 2020/9/15.
+ */
+public class SyncUserInfoTask extends  Thread {
+
+    //定时任务发送数据到上位机
+    @Override
+    public void run()
+    {
+        List<VSysUser> list = VSysUser.dao.find("select * from "+ TableList.SYS_USER + " where isSync != 'T'");
+        String token = SSOService.me.getToken();
+        for(VSysUser record : list)
+        {
+            String username = record.getUsername();
+            String password= "000000";
+            String name= record.getTrueName();
+            String phone= record.getTel();
+            String idCard= record.getIdCard();
+            String organCode= null;
+            boolean result = SSOService.me.userSync(token,username,password,name,phone,idCard,organCode);
+            if(result)
+            {
+                record.setIsSync("T");
+                record.update();
+            }
+        }
+    }
+
+}
