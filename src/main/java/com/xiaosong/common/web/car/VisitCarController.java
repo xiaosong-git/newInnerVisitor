84d411a0a90c491065811868bd7716d577a63338
走走停停 <791618770@qq.com>
2021-01-27 16:33:07 +0800

    1、来访车辆管理

---
+++ src/main/java/com/xiaosong/common/web/car/VisitCarController.java
@@ -0,0 +1,67 @@
+package com.xiaosong.common.web.car;
+
+import com.jfinal.core.Controller;
+import com.jfinal.log.Log;
+import com.jfinal.plugin.activerecord.Page;
+import com.xiaosong.interceptor.jsonbody.JsonBody;
+import com.xiaosong.model.VCar;
+import com.xiaosong.util.RetUtil;
+
+/**
+ * @Author: gx
+ * @Date: 2021/1/27 14:29
+ * @Description: 来访车辆管理
+ */
+public class VisitCarController extends Controller {
+
+    private Log log = Log.getLog(VisitCarController.class);
+
+    private VisitCarService visitCarService = VisitCarService.me;
+
+    /**
+     * 来访车辆管理列表
+     */
+    public void getVisitCarList() {
+        try {
+            int currentPage = getInt("currentPage");
+            int pageSize = getInt("pageSize");
+            Page<VCar> visitCarList = visitCarService.getVisitCarList(currentPage, pageSize, get("phone"), get("cStatus"));
+            renderJson(RetUtil.ok(visitCarList));
+        } catch (Exception e) {
+            log.error("错误信息：", e);
+            renderJson(RetUtil.fail(e.getCause().getLocalizedMessage()));
+        }
+    }
+
+    /**
+     * 来访车辆记录审核
+     */
+    public void auditVisitCar() {
+        try {
+            if (visitCarService.auditVisitCar(getLong("id"), get("cStatus")) > 0) {
+                renderJson(RetUtil.ok());
+            } else {
+                renderJson(RetUtil.fail());
+            }
+        } catch (Exception e) {
+            log.error("错误信息：", e);
+            renderJson(RetUtil.fail(e.getCause().getLocalizedMessage()));
+        }
+    }
+
+    /**
+     * 新增来访车辆记录
+     */
+    public void insertVisitCar(@JsonBody VCar vCar) {
+        try {
+            if (vCar == null || vCar._getAttrNames().length == 0) {
+                renderJson(RetUtil.fail("参数缺失！"));
+            } else {
+                renderJson(visitCarService.insertVisitCar(vCar));
+            }
+        } catch (Exception e) {
+            log.error("错误信息：", e);
+            renderJson(RetUtil.fail(e.getCause().getLocalizedMessage()));
+        }
+    }
+}
---
+++ src/main/java/com/xiaosong/common/web/car/VisitCarService.java
@@ -0,0 +1,47 @@
+package com.xiaosong.common.web.car;
+
+import com.jfinal.plugin.activerecord.Db;
+import com.jfinal.plugin.activerecord.Page;
+import com.xiaosong.constant.Constant;
+import com.xiaosong.model.VCar;
+import com.xiaosong.util.DateUtil;
+import com.xiaosong.util.RetUtil;
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * @Author: gx
+ * @Date: 2021/1/27 14:31
+ * @Description: 来访车辆管理实现类
+ */
+public class VisitCarService {
+
+    public static final VisitCarService me = new VisitCarService();
+
+    public Page<VCar> getVisitCarList(int currentPage, int pageSize, String phone, String cStatus) {
+        StringBuilder sql = new StringBuilder("  from v_car");
+        StringBuilder whereSql = new StringBuilder(" where 1=1 ");
+        if (StringUtils.isNotBlank(phone)) {
+            whereSql.append(" and phone like CONCAT('%',").append(phone).append(",'%')");
+        }
+        if (StringUtils.isNotBlank(cStatus)) {
+            whereSql.append(" and cStatus =").append(cStatus);
+        }
+        whereSql.append(" order by id ");
+        return VCar.dao.paginate(currentPage, pageSize, "select *", sql.append(whereSql).toString());
+    }
+
+    public int auditVisitCar(Long id, String cStatus) {
+        return Db.update("update v_car set cStatus=? where id=? and cStatus = applyConfirm ", cStatus, id);
+    }
+
+    public RetUtil insertVisitCar(VCar vCar) {
+        vCar.setCStatus("applySuccess");
+        vCar.setVisitDate(DateUtil.getCurDate());
+        vCar.setVisitTime(DateUtil.getCurTime());
+        vCar.setReplyDate(DateUtil.getCurDate());
+        vCar.setReplyTime(DateUtil.getCurTime());
+        vCar.setRecordType(Constant.INVITE);
+        boolean save = vCar.save();
+        return save ? RetUtil.ok("新增成功") : RetUtil.fail("新增失败");
+    }
+}
