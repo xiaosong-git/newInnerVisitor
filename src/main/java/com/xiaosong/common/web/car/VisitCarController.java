97b2c3ee7fc53208ad2621c458a719f0af1da287
走走停停 <791618770@qq.com>
2021-01-29 16:42:20 +0800

    1、来访车辆接口修改

--- src/main/java/com/xiaosong/common/web/car/VisitCarController.java
+++ src/main/java/com/xiaosong/common/web/car/VisitCarController.java
@@ -2,10 +2,14 @@ package com.xiaosong.common.web.car;
 
 import com.jfinal.core.Controller;
 import com.jfinal.log.Log;
+import com.jfinal.plugin.activerecord.Db;
 import com.jfinal.plugin.activerecord.Page;
+import com.jfinal.plugin.activerecord.Record;
 import com.xiaosong.interceptor.jsonbody.JsonBody;
 import com.xiaosong.model.VCar;
+import com.xiaosong.util.DESUtil;
 import com.xiaosong.util.RetUtil;
+import org.apache.commons.lang3.StringUtils;
 
 /**
  * @Author: gx
@@ -25,7 +29,12 @@ public class VisitCarController extends Controller {
         try {
             int currentPage = getInt("currentPage");
             int pageSize = getInt("pageSize");
-            Page<VCar> visitCarList = visitCarService.getVisitCarList(currentPage, pageSize, getPara("phone"), getPara("cStatus"));
+            Page<VCar> visitCarList = visitCarService.getVisitCarList(currentPage, pageSize, getPara("plate"), getPara("cStatus"));
+            //获取加密key
+            Record user_key = Db.findFirst("select * from v_user_key");
+            for (VCar vCar : visitCarList.getList()) {
+                vCar.setIdNO(DESUtil.encode(user_key.getStr("workKey"), vCar.getStr("idNO")));
+            }
             renderJson(RetUtil.okData(visitCarList));
         } catch (Exception e) {
             log.error("错误信息：", e);
@@ -38,7 +47,10 @@ public class VisitCarController extends Controller {
      */
     public void auditVisitCar() {
         try {
-            if (visitCarService.auditVisitCar(getLong("id"), getPara("cStatus")) > 0) {
+            if (getLong("id") == null || StringUtils.isEmpty(getPara("cStatus")) || getLong("userId") == null) {
+                renderJson(RetUtil.fail("参数缺失！"));
+            }
+            if (visitCarService.auditVisitCar(getLong("userId"), getLong("id"), getPara("cStatus")) > 0) {
                 renderJson(RetUtil.ok());
             } else {
                 renderJson(RetUtil.fail());
@@ -54,6 +66,9 @@ public class VisitCarController extends Controller {
      */
     public void passVisitCar() {
         try {
+            if (getLong("id") == null) {
+                renderJson(RetUtil.fail("参数缺失！"));
+            }
             if (visitCarService.passVisitCar(getLong("id")) > 0) {
                 renderJson(RetUtil.ok());
             } else {
@@ -70,7 +85,9 @@ public class VisitCarController extends Controller {
      */
     public void insertVisitCar(@JsonBody VCar vCar) {
         try {
-            if (vCar == null || vCar._getAttrNames().length == 0) {
+            if (vCar == null || vCar._getAttrNames().length == 0 ||
+                    StringUtils.isEmpty(vCar.get("plate")) || StringUtils.isEmpty(vCar.get("userName")) || StringUtils.isEmpty(vCar.get("idNO")) ||
+                    StringUtils.isEmpty(vCar.get("startDate")) || StringUtils.isEmpty(vCar.get("endDate")) || vCar.getLong("replyUserId") == null) {
                 renderJson(RetUtil.fail("参数缺失！"));
             } else {
                 renderJson(visitCarService.insertVisitCar(vCar));
--- src/main/java/com/xiaosong/common/web/car/VisitCarService.java
+++ src/main/java/com/xiaosong/common/web/car/VisitCarService.java
@@ -2,8 +2,10 @@ package com.xiaosong.common.web.car;
 
 import com.jfinal.plugin.activerecord.Db;
 import com.jfinal.plugin.activerecord.Page;
+import com.jfinal.plugin.activerecord.Record;
 import com.xiaosong.constant.Constant;
 import com.xiaosong.model.VCar;
+import com.xiaosong.util.DESUtil;
 import com.xiaosong.util.DateUtil;
 import com.xiaosong.util.RetUtil;
 import org.apache.commons.lang3.StringUtils;
@@ -17,11 +19,11 @@ public class VisitCarService {
 
     public static final VisitCarService me = new VisitCarService();
 
-    public Page<VCar> getVisitCarList(int currentPage, int pageSize, String phone, String cStatus) {
+    public Page<VCar> getVisitCarList(int currentPage, int pageSize, String plate, String cStatus) {
         StringBuilder sql = new StringBuilder("  from v_car");
         StringBuilder whereSql = new StringBuilder(" where 1=1 ");
-        if (StringUtils.isNotBlank(phone)) {
-            whereSql.append(" and phone like CONCAT('%','").append(phone).append("','%')");
+        if (StringUtils.isNotBlank(plate)) {
+            whereSql.append(" and plate ='").append(plate).append("'");
         }
         if (StringUtils.isNotBlank(cStatus)) {
             whereSql.append(" and cStatus ='").append(cStatus).append("'");
@@ -30,8 +32,11 @@ public class VisitCarService {
         return VCar.dao.paginate(currentPage, pageSize, "select *", sql.append(whereSql).toString());
     }
 
-    public int auditVisitCar(Long id, String cStatus) {
-        return Db.update("update v_car set cStatus=? where id=? and cStatus = 'applyConfirm' ", cStatus, id);
+    public int auditVisitCar(Long userId, Long id, String cStatus) {
+        String replyDate = DateUtil.getCurDate();
+        String replyTime = DateUtil.getCurTime();
+        return Db.update("update v_car set cStatus=?, replyUserId=?, replyDate=?, replyTime=? where id=? and cStatus = 'applyConfirm' ",
+                cStatus, userId, replyDate, replyTime, id);
     }
 
     public int passVisitCar(Long id) {
@@ -46,6 +51,11 @@ public class VisitCarService {
         vCar.setReplyDate(DateUtil.getCurDate());
         vCar.setReplyTime(DateUtil.getCurTime());
         vCar.setRecordType(Constant.INVITE);
+        //受访人为创建记录的人员
+        vCar.setIntervieweeId(vCar.getLong("replyUserId"));
+        //获取加密key
+        Record user_key = Db.findFirst("select * from v_user_key");
+        vCar.setIdNO(DESUtil.decode(user_key.getStr("workKey"), vCar.getStr("idNO")));
         boolean save = vCar.save();
         return save ? RetUtil.ok("新增成功") : RetUtil.fail("新增失败");
     }
